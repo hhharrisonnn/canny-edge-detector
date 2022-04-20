b41723ecb9549e040c6517bc54ddd01295906772
hhharrisonnn <harrison.ho19@gmail.com>
2022-04-20 01:34:58 +0100

    Added double threshold function

---
+++ src/doublethreshold.go
@@ -0,0 +1,41 @@
+package canny
+
+import (
+	"github.com/hhharrisonnn/canny-edge-detector/imgcreation"
+	"image"
+	"image/color"
+)
+
+func DoubleThreshold(highThresholdRatio float64, lowThresholdRatio float64) {
+	imageIndex, imgWidth, imgHeight := imgcreation.GetGrey("./img/nonmaxsup.png")
+
+	var strong float64 = 255
+	var weak float64 = 25
+
+	highThreshold := strong * highThresholdRatio
+	lowThreshold := highThreshold * lowThresholdRatio
+
+	newImage := image.NewGray((image.Rectangle{image.Point{1, 1}, image.Point{imgWidth - 1, imgHeight - 1}}))
+
+	for j := 1; j < imgHeight; j++ {
+		for i := 1; i < imgWidth; i++ {
+			// 100% an edge
+			if imageIndex[i][j] >= highThreshold {
+				imageIndex[i][j] = strong
+			}
+			// Weak
+			if (imageIndex[i][j] < highThreshold) && (imageIndex[i][j] > lowThreshold) {
+				imageIndex[i][j] = weak
+			}
+			// Not an edge
+			if imageIndex[i][j] <= lowThreshold {
+				imageIndex[i][j] = 0
+			}
+
+			greyColour := color.Gray{uint8(imageIndex[i][j])}
+			newImage.Set(i, j, greyColour)
+		}
+	}
+
+	imgcreation.Encode(newImage, "doublethreshold")
+}
